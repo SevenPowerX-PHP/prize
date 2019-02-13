6865eac2182cc59a09741b24ced70165a67315e3
splaa-Acer <splaandrey@gmail.com>
2019-02-13 16:06:08 +0100

    Added button

--- web/index.php
+++ web/index.php
@@ -5,6 +5,23 @@
 	 * Date: 13.02.2019
 	 * Time: 13:18
 	 */
-	echo 'Hello.world!!';
 
 
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Приз!!!</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<p style="text-align: center">
+	<button class="btn-get-prize">Получить Приз!!!</button>
+</p>
+</body>
+</html>

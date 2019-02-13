0934b8ccf30e31d051d24065aacbcabb997501d1
splaa-Acer <splaandrey@gmail.com>
2019-02-13 15:17:55 +0100

    Added base class Prize

---
+++ classes/Prize.php
@@ -0,0 +1,15 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Lavryniuk Andrii
+	 * Date: 13.02.2019
+	 * Time: 15:13
+	 */
+
+	class Prize
+	{
+		private $prizeName;
+		private $status;
+
+
+	}

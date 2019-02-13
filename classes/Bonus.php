2d28e95b78f81d49b34fb3995bdd7b270a254dc3
splaa-Acer <splaandrey@gmail.com>
2019-02-13 15:02:31 +0100

    Added Bonus class

---
+++ classes/Bonus.php
@@ -0,0 +1,26 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: Lavryniuk Andrii
+	 * Date: 13.02.2019
+	 * Time: 15:01
+	 */
+
+	class Bonus implements Prize
+	{
+
+		public function getPrize(): Prize
+		{
+			// TODO: Implement getPrize() method.
+		}
+
+		public function toSend()
+		{
+			// TODO: Implement toSend() method.
+		}
+
+		public function toConvert()
+		{
+			// TODO: Implement toConvert() method.
+		}
+	}
